221954a7169401ddfc12482c094956bc800031b1
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-11 20:26:39 +0500

    practice code with harry

---
+++ practice-javascript-harry/index.js
@@ -0,0 +1,40 @@
+// question-1
+let name = "maryam";
+let num = 8;
+
+console.log(name + num);
+
+// question-2
+
+console.log(typeof (name+num));
+
+// question-3
+
+const a = {
+    name:"maryam",
+    section: 1,
+    isPrinciple: false,
+}
+
+// a = "b"
+
+
+// question-4
+
+a ["student"]=45
+a ["name"]="Aisha"
+
+console.log(a);
+
+
+
+// question-5
+const dict={
+    stratum:" one of a number of portions or divisions likened to layers or levels.",
+    yakka:"Hard work",
+    worded:"a strongly worded letter",
+    earnest:"characterized by a firm, sincere belief in one's opinions"
+}
+
+console.log(dict.yakka);
+console.log(dict["earnest"]);
